21aad6abce77dc6817b8bb42d641c0e3d2f77c00
Vitaopb <victor.lieuthier@gmail.com>
2021-10-04 22:01:12 -0300

    mais 1

---
+++ WalkingGame-Console/WalkingGame-Console/Cenario.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalkingGame_Console
+{
+    class Cenario
+    {
+        public string Chao { get; set; }
+
+        public Byte Tamanho { get; set; }
+
+        public Cenario(String chao, Byte tamaho)
+        {
+            Chao = chao;
+            Tamanho = tamaho;
+        }
+
+        public void imprime(Player p)
+        {
+            Console.Clear();
+            Console.WriteLine();
+
+            // IMPRIME UMA LINHA
+            if (p.Pulando)
+            {
+                for (Byte i = 0; i < p.Posicao; i++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(p.Aparencia);
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+
+            for (Byte i = 0; i < p.Posicao; i++)
+            {
+                Console.Write(Chao);
+            }
+
+            if (p.Pulando) Console.Write(Chao);
+            else Console.Write(p.Aparencia);
+
+            for (Byte i = 0; i < (Tamanho - p.Posicao); i++)
+            {
+                Console.Write(Chao);
+            }
+        }
+    }
+}
---
+++ WalkingGame-Console/WalkingGame-Console/Player.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalkingGame_Console
+{
+    class Player
+    {
+
+        public Byte Posicao { get; set; }
+        public String Aparencia { get; set; }
+        public String FacingRight { get; set; }
+        public String FacingLeft { get; set; }
+        public Boolean Pulando { get; set; }
+
+        public Player(String right, String left)
+        {
+            Posicao = 0;
+            FacingLeft = left;
+            FacingRight = right;
+            Aparencia = FacingRight;
+            Pulando = false;
+        }
+
+        public void pula()
+        {
+            Console.Beep(234, 100);
+        }
+
+        public void anda(Boolean direcao, Byte limite)
+        {
+            if (direcao)
+            {
+                if (Posicao < limite)
+                {
+                    Posicao++;
+                    Aparencia = FacingRight;
+                }
+                else
+                {
+                    Posicao--;
+                    Aparencia = FacingLeft;
+                }
+            }
+        }
+    }
+}
